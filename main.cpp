9b3075e744f4b9882adfc352eed9f178e5fa4b82
Turupawn <ahmed.hn.43@gmail.com>
2014-11-18 08:43:17 -0600

    Primera clase

--- main.cpp
+++ main.cpp
@@ -8,8 +8,19 @@ int main()
 {
     XMLDocument doc;
     doc.LoadFile("test.xml");
-    XMLElement* titleElement = doc.FirstChildElement( "A" )->FirstChildElement( "B" );
-    const char* ejemplo = titleElement->GetText();
-    cout<<"Leyendo XML: "<<ejemplo;
+    XMLElement* pasteles = doc.FirstChildElement("Pasteles");
+    for(XMLElement* pastel = pasteles->FirstChildElement("Pastel");
+            pastel!=NULL;
+            pastel=pastel->NextSiblingElement())
+    {
+        cout<<"-------------------------"<<endl;
+        cout<<pastel->Attribute("nombre")<<endl;
+        cout<<pastel->Attribute("esta_vencido")<<endl;
+        cout<<pastel->Attribute("cantidad")<<endl;
+    }
+
+//    XMLElement* titleElement = doc.FirstChildElement( "A" )->FirstChildElement( "B" );
+//    const char* ejemplo = titleElement->GetText();
+//    cout<<"Leyendo XML: "<<ejemplo;
     return 0;
 }
